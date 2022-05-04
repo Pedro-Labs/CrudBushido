fd82dd3d8a79893b0b321d920a5b62b42a6f7629
Joao Pedro <ferreirap.pedro233@gmail.com>
2022-05-04 16:20:20 -0300

    teste funcional

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "12345678",
-    DB: "database"
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b52f1d4e4b7fb0",
+    PASSWORD: "7b6a3986",
+    DB: "heroku_d6b0e0f51e87398"
   };

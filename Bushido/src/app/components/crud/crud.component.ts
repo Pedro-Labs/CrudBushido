4983962e670bda684681cbb7071ecbae8cfd4861
Joao Pedro <ferreirap.pedro233@gmail.com>
2022-06-16 14:57:09 -0300

    Versão Final

--- Bushido/src/app/components/crud/crud.component.ts
+++ Bushido/src/app/components/crud/crud.component.ts
@@ -5,8 +5,8 @@ import { produtos, users } from './model/getmodel';
 import { MenuItem } from 'primeng/api';
 
 
-@Component({ 
- selector: 'app-crud',
+@Component({
+  selector: 'app-crud',
   templateUrl: './crud.component.html',
   styleUrls: ['./crud.component.css']
 })
@@ -14,55 +14,55 @@ import { MenuItem } from 'primeng/api';
 
 export class CrudComponent implements OnInit {
   isButtonVisible!: boolean;
-  
+
   items!: MenuItem[];
 
-  display!: boolean ;
+  display!: boolean;
   displayBasic!: boolean;
   displayPosition!: boolean;
   displayMaximizable!: boolean;
-  
+
   messageService: any;
   position!: string;
   value3!: string;
-  
+
   token!: string
   message!: boolean;
 
   buttonVisible!: boolean
 
-  showButton(){
+  showButton() {
     this.buttonVisible = true
   }
 
   showMessage() {
     this.display = true;
-}
-
-    showDialog(produto: string, preco: string, descricao: string, id: number) {
-      this.inputProduto = produto;
-      this.inputPreco = preco;
-      this.inputDescricao = descricao;
-      this.inputId = id;
-      
-      this.display = true;
-    }
-    
-    showBasicDialog() {
-      
-      this.displayBasic = true;
-    }
-
-    showMaximizableDialog() {
-      this.displayMaximizable = true;
   }
-    
-    showPositionDialog(position: string , id: number) {
-  
+
+  showDialog(produto: string, preco: string, descricao: string, id: number) {
+    this.inputProduto = produto;
+    this.inputPreco = preco;
+    this.inputDescricao = descricao;
+    this.inputId = id;
+
+    this.display = true;
+  }
+
+  showBasicDialog() {
+
+    this.displayBasic = true;
+  }
+
+  showMaximizableDialog() {
+    this.displayMaximizable = true;
+  }
+
+  showPositionDialog(position: string, id: number) {
+
     this.inputId = id;
     this.displayPosition = true;
     this.position = position;
-}
+  }
 
 
   url = 'http://localhost:8080'
@@ -88,7 +88,7 @@ export class CrudComponent implements OnInit {
   //Paginação
   totalRecords!: number;
   cols!: any[];
-  
+
   loginVisible!: boolean
 
 
@@ -96,13 +96,13 @@ export class CrudComponent implements OnInit {
   constructor(private _route: Router, private _httpclient: HttpClient,) {
     this.usuario = [];
     this.produto = [];
-   }
+  }
 
 
-  Search(){
-    if(this.searchInput == ""){
+  Search() {
+    if (this.searchInput == "") {
       this.getAll(); //this.ngOnInit()
-    }else{
+    } else {
       this.produto = this.produto.filter(res => {
         return res.produto.toLowerCase().match(this.searchInput.toLowerCase());
       })
@@ -110,13 +110,14 @@ export class CrudComponent implements OnInit {
   }
 
 
-   //Função exibir produtos por Ordem Alfabética
+  //Função exibir produtos por Ordem Alfabética
   getAll() {
-    return this._httpclient.get(this.url+ '/api/tutorials', {headers: new HttpHeaders({'x-auth-token': this.token}) }).subscribe((result: any) => {
+    return this._httpclient.get(this.url + '/api/tutorials', { headers: new HttpHeaders({ 'x-auth-token': this.token }) }).subscribe((result: any) => {
       this.produto = [];
-      for (var item of result){
-        
-        this.produto.push({id: item.id, produto: item.produto, descricao: item.descricao, preco: item.preco
+      for (var item of result) {
+
+        this.produto.push({
+          id: item.id, produto: item.produto, descricao: item.descricao, preco: item.preco
         })
       }
       this.isButtonVisible = true;
@@ -124,61 +125,68 @@ export class CrudComponent implements OnInit {
     })
   }
 
+
   //Exibir preço MAIOR
   getExp() {
-    return this._httpclient.get(this.url+ '/api/tutorials/exp', {headers: new HttpHeaders({'x-auth-token': this.token}) }).subscribe((result: any) => {
+    return this._httpclient.get(this.url + '/api/tutorials/exp', { headers: new HttpHeaders({ 'x-auth-token': this.token }) }).subscribe((result: any) => {
       this.produto = [];
-      for (var item of result){
-        
-        this.produto.push({id: item.id, produto: item.produto, descricao: item.descricao, preco: item.preco
+      for (var item of result) {
+
+        this.produto.push({
+          id: item.id, produto: item.produto, descricao: item.descricao, preco: item.preco
         })
       }
     })
   }
+
 
   //Exibir preço MENOR
   getCheap() {
-    return this._httpclient.get(this.url+ '/api/tutorials/cheap', {headers: new HttpHeaders({'x-auth-token': this.token}) }).subscribe((result: any) => {
+    return this._httpclient.get(this.url + '/api/tutorials/cheap', { headers: new HttpHeaders({ 'x-auth-token': this.token }) }).subscribe((result: any) => {
       this.produto = [];
-      for (var item of result){
-        
-        this.produto.push({id: item.id, produto: item.produto, descricao: item.descricao, preco: item.preco
+      for (var item of result) {
+
+        this.produto.push({
+          id: item.id, produto: item.produto, descricao: item.descricao, preco: item.preco
         })
       }
     })
   }
 
-   //Função excluir produtos
+
+  //Função excluir produtos
   delete(id: number) {
-     this._httpclient.delete(this.url + `/api/tutorials/${id}`,  {headers: new HttpHeaders({'x-auth-token': this.token}) }).subscribe((result: any) => {
+    this._httpclient.delete(this.url + `/api/tutorials/${id}`, { headers: new HttpHeaders({ 'x-auth-token': this.token }) }).subscribe((result: any) => {
       // if(!result.ok) {
       // alert('Sessão expirada.. ou token inválido!')
       // }else{
-        
+
       // }
       this.getAll();
       this.displayPosition = false;
     })
-    
+
   }
-  
+
+
   //Função atualizar produtos
   update(produto: string, preco: string, descricao: string, id: number) {
-    this._httpclient.put<any>(this.url + `/api/tutorials/${id}`, {produto:produto, descricao: descricao, preco:preco, id:id }, {headers: new HttpHeaders({'x-auth-token': this.token}) }).subscribe((result: any) => {
+    this._httpclient.put<any>(this.url + `/api/tutorials/${id}`, { produto: produto, descricao: descricao, preco: preco, id: id }, { headers: new HttpHeaders({ 'x-auth-token': this.token }) }).subscribe((result: any) => {
       // if(!result.ok) {
       // alert("Sessão expirada.. ou token inválido!")
       //   }else{
-          
+
       //   }
       this.getAll();
       this.display = false
     })
   }
 
+
   //Função Novo Produto
   new(produto: string, preco: string, descricao: string) {
-    this._httpclient.post<any>(this.url + '/api/tutorials', {produto:produto, descricao: descricao, preco:preco},{headers: new HttpHeaders({'x-auth-token': this.token}) } ).subscribe((result: any) => {
-      
+    this._httpclient.post<any>(this.url + '/api/tutorials', { produto: produto, descricao: descricao, preco: preco }, { headers: new HttpHeaders({ 'x-auth-token': this.token }) }).subscribe((result: any) => {
+
       this.produto = [];
       this.getAll();
       this.inputProduto2 = "";
@@ -187,21 +195,23 @@ export class CrudComponent implements OnInit {
       this.displayBasic = false;
     })
   }
-  
+
+
   //Função Login
   login(email: string, password: string) {
-    this._httpclient.post<any>(this.url + '/api/auth', {email:email, password: password }).subscribe((result:any) => {
+    this._httpclient.post<any>(this.url + '/api/auth', { email: email, password: password }).subscribe((result: any) => {
       this.token = result.token;
       this.usuario = [];
       this.inputEmail = "";
       this.inputPassword = "";
       this.displayMaximizable = false;
       this.getAll();
-      
-      
-    })
-  } 
 
+
+    })
+  }
+
+  
   //Funções de navegação (desuso)
   navigateTo() {
     this._route.navigate(['/create-product'])
@@ -211,21 +221,25 @@ export class CrudComponent implements OnInit {
     this._route.navigate(['/update-product'])
   }
 
-  
+
 
   ngOnInit(): void {
 
-     this.loginVisible = true
+    this.loginVisible = true
 
     this.items = [
-      {label: 'Maior preço', icon: 'pi pi-dollar', command: () => {
+      {
+        label: 'Maior preço', icon: 'pi pi-dollar', command: () => {
           this.getExp();
-      }},
+        }
+      },
 
-      {label: 'Menor preço', icon: 'pi pi-dollar', command: () => {
+      {
+        label: 'Menor preço', icon: 'pi pi-dollar', command: () => {
           this.getCheap();
-      }},
-  ];
+        }
+      },
+    ];
 
 
     this.getAll();
@@ -237,5 +251,5 @@ export class CrudComponent implements OnInit {
     this.totalRecords = this.produto.length
   }
 
-  
+
 }
